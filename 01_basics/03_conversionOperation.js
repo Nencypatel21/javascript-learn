0409fbc0c0a57a999c61e206cd64b1816e8b242f
nencypatel <ndpatelsokhada2006@gmail.com>
2026-07-31 05:56:34 +0000

    added notes for type conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,8 @@
-let score = "false"
+// let score = null
+// let score = undefined
+// let score = "33abc"
+let score = 33
+// let score = true
 
 console.log(score);
 
@@ -9,17 +13,23 @@ let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber); //NaN
 
+//  Number Conversion
 // "33" => 33
 // "33abc" => NaN but still typeof syas its a number
 // true => 1; false => 0
+// null => 0 and and typeof says its a number
+// undefined => NaN but still typeof says its a number
 
-let isLoggedIn = "n"
+let isLoggedIn = undefined
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 
+// Boolean Conversion
 // 1 => true; 0 => false
 // "" => false
 // "n" => true
+// null => false
+// undefined => false
 
 let someNumber = 33
 let stringNumber = String(someNumber)
