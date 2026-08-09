ad34c5fc8644e61e4fc918a378a45ebb14d8045e
nencypatel <ndpatelsokhada2006@gmail.com>
2026-08-09 17:54:50 +0000

    more details of scope in javascript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,6 +8,45 @@ if(true){
 }
 
 console.log(a);
-// console.log(b);
-// console.log(c);
+// console.log(b); // gives error
 
+
+function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website); // gives error
+    two()
+}
+
+one()
+
+if(true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);  // gives error
+    
+}
+// console.log(username);  // gives error
+
+
+// +++++++++++++++++ Interesting +++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+console.log(addone(5))
+
+
+// another way of creating a function 
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))
